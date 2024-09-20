4ba4a821b3dd9514e8fa80ebfae69ad4441f8647
Gabriel <Gabriel.bachelor@gmail.com>
2024-09-20 10:01:25 +0000

    mod3

--- rick.ts
+++ rick.ts
@@ -28,12 +28,12 @@
   fetch("https://rickandmortyapi.com/api/character")
     .then((r) => r.json())
     .then((json: { info: Info; results: Character[] }) => {
-      for (let i = 0; i < json.results.length; i++) {
-        console.log(json.results[i].name + " is in ");
-        console.log(json.results[i].episode.length + " episodes");
-        console.log("Character specie is " + json.results[i].species);
-        console.log("Origin " + json.results[i].origin.name);
-        console.log("Gender " + json.results[i].gender);
+      for (let i = 0; i < json.results.length; i++) {    //loops through the whole length of #results
+        console.log(json.results[i].name + " is in ");  //logs all names + ' is in ' string
+        console.log(json.results[i].episode.length + " episodes");  // same as above
+        console.log("Character specie is " + json.results[i].species); // loops through species
+        console.log("Origin " + json.results[i].origin.name);  // loops through origin
+        console.log("Gender " + json.results[i].gender);  // loops through gender
       }
     });
 }
